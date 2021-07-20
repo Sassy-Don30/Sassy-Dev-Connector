2c39a2b34eaabfe21c3f0c2bda14f1bc9e45946d
Sassy-Don30 <shashwat.tandon30@gmail.com>
2021-07-20 14:46:13 +0530

    Deploy ready

---
+++ client/build/precache-manifest.d5320a16b47aff0d35e3cbacb3a8796f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c672f26e7985ab7bcad3459c6c5cada",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49a171c0bbfa440a255b",
+    "url": "/static/css/main.a59fb7c2.chunk.css"
+  },
+  {
+    "revision": "04ac6740a0e30b5cf28a",
+    "url": "/static/js/2.a0cadca4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a0cadca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49a171c0bbfa440a255b",
+    "url": "/static/js/main.4811468d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
